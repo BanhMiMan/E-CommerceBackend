d8a9d604735cc1319d02675e2c20c07fecba05da
iammrmoo@gmail.com <iammrmoo@gmail.com>
2023-08-07 00:22:56 -0400

    git repo

---
+++ Develop/routes/api/tag-routes.js
@@ -0,0 +1,82 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', async (req, res) => {
+  // find all tags
+  // be sure to include its associated Product data
+  try {
+    const allTags = await Tag.findAll({
+      include: [{ model: Product}],
+    });
+    res.status(200).json(allTags);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  // find a single tag by its `id`
+  // be sure to include its associated Product data
+  try {
+    const oneTag = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product}],
+    });
+    if (!oneTag) {
+      res.status(404).json({ message: 'Tag id not found' });
+      return;
+    }
+    res.status(200).json(oneTag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+  // create a new tag
+  try {
+    const createTag = await Tag.create(req.body);
+    res.status(200).json(createTag);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  // update a tag's name by its `id` value
+  try {
+    const updateTag = await Tag.update(req.body, {
+      where: { id: req.params.id }, individualHooks: true
+    });
+    if (!updateTag) {
+      res.status(404).json({ message: 'Tag id not found' });
+      return;
+    }
+    res.status(200).json(updateTag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  // delete on tag by its `id` value
+  try {
+    const deleteTag = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!deleteTag) {
+      res.status(404).json({ message: 'Tag id not found' });
+      return;
+    }
+    res.status(200).json(deleteTag);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
+ 
